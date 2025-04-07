b74c6aa3210fbb12e95f4189008087004c988a49
aka: cauzality, zeno evil, ... <cauzality@comcast.net>
2025-04-07 10:45:18 -0500

    readying for vercel

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173',process.env.REACT_URL],
+    origin: process.env.REACT_URL,
 }))
 
 
