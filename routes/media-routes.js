13ac749aaddf402fbc0a793ba08e378a82bb8b63
aka: cauzality, zeno evil, ... <cauzality@comcast.net>
2025-04-07 10:03:41 -0500

    readying for vercel

--- routes/media-routes.js
+++ routes/media-routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const multer = require("multer");
+//const multer = require("multer");
 const { uploadMediaToCloudinary } = require("../helpers/cloudinary");
 
 
-const upload = multer({dest: 'uploads/'})
+//const upload = multer({dest: 'uploads/'})
 const router = express.Router()
 
 // router.post('/upload', upload.single('file'), async (req,res)=>{
