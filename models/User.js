1c399056827896021e0bcfe947aee6a5247aa3e9
aka: cauzality, zeno evil, ... <cauzality@comcast.net>
2025-04-18 00:51:59 -0500

    fixing for vercel again

--- models/User.js
+++ models/User.js
@@ -24,7 +24,8 @@ const UserSchema = new Schema({
         required: true
     },
     secondEmail: {
-        type: String
+        type: String,
+        default: null
     },
     role: {
         type: String,
